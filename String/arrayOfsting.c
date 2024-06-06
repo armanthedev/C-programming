9d8c8e46b20bc9ee4336b6eb44becf191ad0dfaf
armanthedev <armanhosennobel@gmail.com>
2024-06-07 03:44:26 +0600

    codeforece problem solving

--- String/arrayOfsting.c
+++ String/arrayOfsting.c
@@ -40,19 +40,39 @@ int main()
     // fgets(D, 6, stdin);
     // printf("%s", D);
 
-    char f;
-    scanf("%s", &f);
-    int ascii_value = (int)f;
-    printf("print character: %c\n", f);
-    printf("Print ascii valu of %c: %d\n", f, ascii_value);
+    // char f;
+    // scanf("%s", &f);
+    // int ascii_value = (int)f;
+    // printf("print character: %c\n", f);
+    // printf("Print ascii valu of %c: %d\n", f, ascii_value);
 
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
+    // printf("\n");
+    // printf("\n");
+    // printf("\n");
+    // printf("\n");
 
-    char as[] = {'3'};
-    printf("ascii valu of %c is %d", as, as);
+    // char as[] = {'3'};
+    // printf("ascii valu of %c is %d", as, as);
+    char s[1000001];
+    scanf("%s", s);
+    int sum = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+
+        sum = sum + s[i] - '0'; //'0' means =48 which is ascii value;
+
+        /*
+         loop 0 tomo index er value er  ascii value  minus hobe 48
+         for example 0 tomo index character = 8, jar ascii value 56
+         so 56 -48 = 8;
+
+        */
+
+        // printf("%d ", s[i]);
+        // value = (int)s[i];
+        // printf("%d ", value); // Print the integer value of each character
+    }
+    printf("%d", sum);
 
     return 0;
 }
